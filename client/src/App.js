bd2efe31bd348b81bafe3b2bc87fb9f3218b281a
Revina-M <revina95rm@gmail.com>
2022-03-13 15:00:39 +0530

    cc
    cc

--- client/src/App.js
+++ client/src/App.js
@@ -44,8 +44,8 @@ function App() {
 
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="/login" exact element={<Login />} />
-          <Route path="/register" exact element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route
             path="/appliedjobs"
             exact
