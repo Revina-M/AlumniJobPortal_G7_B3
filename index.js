83f19a255336bcf7230b3da74093f267a04cff30
Revina-M <revina95rm@gmail.com>
2022-03-13 12:21:28 +0530

    cc
    cc

--- index.js
+++ index.js
@@ -14,13 +14,19 @@ app.use("/api/users", usersRoute);
 
 // -------deployment------
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build/index.html"));
+//   });
+// }
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 // __dirname = path.resolve();
 // if (process.env.NODE_ENV === "production") {
